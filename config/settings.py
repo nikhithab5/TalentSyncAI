9643b714ffd63b79670ad78d43df5c7a486fcc10
Nikhitha <nikhithapragallapati128@gmail.com>
2026-06-30 21:29:34 +0530

    Prepare project for Render deployment

--- config/settings.py
+++ config/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -26,13 +27,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--w1ra)o-(58m_nac8s(_ln(8!4nv3ty-xmsn#udmw8_d=x*)uo'
-
+SECRET_KEY = os.getenv(
+    "SECRET_KEY",
+    "django-insecure--w1ra)o-(58m_nac8s(_ln(8!4nv3ty-xmsn#udmw8_d=x*)uo"
+)
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+]
 
 # Application definition
 
@@ -95,6 +100,9 @@ SOCIALACCOUNT_PROVIDERS = {
 }
 MIDDLEWARE = [
 'django.middleware.security.SecurityMiddleware',
+
+'whitenoise.middleware.WhiteNoiseMiddleware',
+
 'django.contrib.sessions.middleware.SessionMiddleware',
 'allauth.account.middleware.AccountMiddleware',
 
@@ -130,11 +138,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=os.getenv("DATABASE_URL"),
+        conn_max_age=600,
+    )
 }
 
 
@@ -172,13 +182,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "/static/"
+
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    BASE_DIR / "static",
 ]
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # Email Configuration
 # Email Configuration
 
