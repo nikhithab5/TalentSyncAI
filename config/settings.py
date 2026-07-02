9f0494ad909202b50a50b15c13d101eaed818b0c
Nikhitha <nikhithapragallapati128@gmail.com>
2026-07-02 11:44:04 +0530

    Updated requirements for deployment

--- config/settings.py
+++ config/settings.py
@@ -34,9 +34,9 @@ SECRET_KEY = os.getenv(
 DEBUG = os.getenv("DEBUG", "False").lower() == "true"
 
 ALLOWED_HOSTS = [
-    ".onrender.com",
     "127.0.0.1",
     "localhost",
+    ".onrender.com",
 ]
 
 # Application definition
@@ -139,9 +139,9 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
