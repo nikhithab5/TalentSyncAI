266369153165fddf7368eeb6e6cbed2d5c626d07
Nikhitha <nikhithapragallapati128@gmail.com>
2026-07-21 14:50:39 +0530

    Fix Django admin URL configuration

--- config/urls.py
+++ config/urls.py
@@ -1,62 +1,53 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.sitemaps.views import sitemap
+
 from core.sitemaps import StaticViewSitemap
 from core.views import robots_txt
+
+
 # -------- TalentSync AI Admin Branding --------
+
 admin.site.site_header = "TalentSync AI Administration"
 admin.site.site_title = "TalentSync AI Admin Portal"
 admin.site.index_title = "Welcome to TalentSync AI Dashboard"
-# --
+
+
 sitemaps = {
     "static": StaticViewSitemap,
 }
 
 
 urlpatterns = [
-     path("robots.txt", robots_txt),
+    path("robots.txt", robots_txt),
 
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
 
-    path('', include('core.urls')),
+    path("", include("core.urls")),
 
-    path('accounts/', include('accounts.urls')),
+    path("accounts/", include("accounts.urls")),
 
-    path('accounts/', include('allauth.urls')),
+    path("accounts/", include("allauth.urls")),
 
     # Password reset URLs
     path(
-        'accounts/',
-        include('django.contrib.auth.urls')
+        "accounts/",
+        include("django.contrib.auth.urls"),
     ),
+
     path(
-    "sitemap.xml",
-    sitemap,
-    {"sitemaps": sitemaps},
-    name="django.contrib.sitemaps.views.sitemap",
-),
+        "sitemap.xml",
+        sitemap,
+        {"sitemaps": sitemaps},
+        name="django.contrib.sitemaps.views.sitemap",
+    ),
 ]
+
 
 if settings.DEBUG:
     urlpatterns += static(
         settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT
+        document_root=settings.MEDIA_ROOT,
     )
