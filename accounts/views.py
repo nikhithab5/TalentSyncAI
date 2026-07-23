1b633c47653efa0d6093df3c4ba6bdf28699175b
Nikhitha <nikhithapragallapati128@gmail.com>
2026-07-23 10:26:41 +0530

    Restore login view

--- accounts/views.py
+++ accounts/views.py
@@ -53,8 +53,21 @@ TalentSyncAI Team
 
 
 def user_login(request):
-    return render(request, "login.html")
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
 
+        user = authenticate(
+            request,
+            username=username,
+            password=password
+        )
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+
+    return render(request, 'login.html')
 
 def user_logout(request):
 
