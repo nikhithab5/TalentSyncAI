28d2cf90de08b56253750589058644f25440fd54
Nikhitha <nikhithapragallapati128@gmail.com>
2026-08-03 10:45:03 +0530

    Fixed signup login backend

--- accounts/views.py
+++ accounts/views.py
@@ -68,7 +68,11 @@ def signup(request):
             password=password
         )
 
-        login(request, user)
+        login(
+    request,
+    user,
+    backend="django.contrib.auth.backends.ModelBackend"
+)
 
         return redirect("home")
 
